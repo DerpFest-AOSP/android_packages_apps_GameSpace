LMODroid/platform_packages_apps_GameSpace

GameSpace: Disable edge cutout by default
Change-Id: Iace26c3d57f422b67446251ea8ebe7ddde1fe6e7

app/src/main/java/io/chaldeaprjkt/gamespace/data/SystemSettings.kt
@@ -110,13 +110,13 @@ class SystemSettings @Inject constructor(
 
     var edgeCutout
         get() = Settings.Secure.getIntForUser(
-            resolver, LMOSettings.Secure.EDGE_CUTOUT, 1,
+            resolver, LMOSettings.Secure.EDGE_CUTOUT, 0,
             UserHandle.USER_CURRENT
         ) == 1
-        set(it) {
+        set(value) {
             Settings.Secure.putIntForUser(
                 resolver, LMOSettings.Secure.EDGE_CUTOUT,
-                it.toInt(), UserHandle.USER_CURRENT
+                value.toInt(), UserHandle.USER_CURRENT
             )
         }
 

DerpFest-AOSP/android_packages_apps_GameSpace

app/src/main/java/io/chaldeaprjkt/gamespace/data/SystemSettings.kt
@@ -121,13 +121,13 @@ class SystemSettings @Inject constructor(
 
     var edgeCutout
         get() = Settings.Secure.getIntForUser(
-            resolver, Settings.Secure.EDGE_CUTOUT, 1,
+            resolver, Settings.Secure.EDGE_CUTOUT, 0,
             UserHandle.USER_CURRENT
         ) == 1
-        set(it) {
+        set(value) {
             Settings.Secure.putIntForUser(
                 resolver, Settings.Secure.EDGE_CUTOUT,
-                it.toInt(), UserHandle.USER_CURRENT
+                value.toInt(), UserHandle.USER_CURRENT
             )
         }
 